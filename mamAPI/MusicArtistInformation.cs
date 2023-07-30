f430984f71436947f8c3482957d58dc980305928
BongaRoberta <bonga.mokoena@kcpivot.com>
2023-07-31 00:49:11 +0200

    Minimized Class Attributes

--- mamAPI/MusicArtistInformation.cs
+++ mamAPI/MusicArtistInformation.cs
@@ -4,14 +4,9 @@
     {
         public int IdNumber { get; set; }
         public string Title { get; set; }
-        public string FirstName { get; set; } = string.Empty;
+        public string FirstName { get; set; }
         public string PreferedName { get; set; }
         public string LastName { get; set; }
-        public string BiographicalDescription { get; set; }
-        public int CellPhoneNumber { get; set; }
-        public char EmailAddress { get; set; }
-        public string PlaceOfResidence { get; set; }
-
 
     }
 }
