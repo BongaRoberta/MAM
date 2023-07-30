50e350c21efa3d3ee7927de5eda03b9e984d4f74
BongaRoberta <bonga.mokoena@kcpivot.com>
2023-07-31 00:20:08 +0200

    Added Music Artist Information Class

---
+++ mamAPI/MusicArtistInformation.cs
@@ -0,0 +1,17 @@
+﻿namespace mamAPI
+{
+    public class MusicArtistInformation
+    {
+        public int IdNumber { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string PreferedName { get; set; }
+        public string LastName { get; set; }
+        public string BiographicalDescription { get; set; }
+        public int CellPhoneNumber { get; set; }
+        public char EmailAddress { get; set; }
+        public string PlaceOfResidence { get; set; }
+
+
+    }
+}
