3156b03b1b78cc2b038ce61135f1bd759cc38cbc
BongaRoberta <bonga.mokoena@kcpivot.com>
2023-09-14 11:42:41 +0200

    model naming conversion refactored

--- mamAPI/Models/MusicArtistInformation.cs
+++ mamAPI/Models/MusicArtistInformation.cs
@@ -2,20 +2,20 @@
 
 public class MusicArtistInformation
 {
-    public int artist_id { get; set; }
-    public string? id_number { get; set; }
-    public string? first_name { get; set; }
-    public string? stage_name { get; set; }
-    public string? last_name { get; set; }
-    public string? gender { get; set; }
-    public string? city_based { get; set; }
-    public string? contact_number { get; set; }
-    public string? email_address { get; set; }
-    public string? instagram_handle { get; set; }
-    public string? tiktok_handle { get; set; }
-    public string? music_genre_style { get; set; }
-    public string? brief_artist_description { get; set; }
-    public string? past_perfomance { get; set; }
-    public byte[]? photograph { get; set; }
+    public int ArtistId { get; set; }
+    public string IdNumber { get; set; }
+    public string FirstName { get; set; }
+    public string StageName { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; }
+    public string CityBased { get; set; }
+    public string ContactNumber { get; set; }
+    public string EmailAddress { get; set; }
+    public string InstagramHandle { get; set; }
+    public string TikTokHandle { get; set; }
+    public string MusicGenreStyle { get; set; }
+    public string BriefArtistDescription { get; set; }
+    public string PastPerformance { get; set; }
+    public byte[] Photograph { get; set; }
 }
 
